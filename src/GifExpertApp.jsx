d92d2af085fac4c80f01cdee85943ebf5d6edb48
Ignacio Alvarado <ignacio.alvarado@uv.cl>
2023-07-25 12:50:34 -0400

    Enviado formulario  a componente padre

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = () => {
+    /* const anadir = setCategories(categories => categories.concat('Valorant')); */
+   /*  const anadir = setCategories([...categories,'Valorant']); */
+      setCategories([...categories,'Valorant']);
+  }
+
   return (
     <>
+   {/*  titulo */}
+   
     <h1>GifExpertApp</h1>
+    <AddCategory setCategories={setCategories}/>
+    <button onClick={onAddCategory}>Agregar</button>
+    <ol> {/*  Order list que crea como un listado con numeros */}
+      {categories.map((index) => <li key={index}> {index} </li>)}
+
+    </ol>
     </>
   )
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+
+export const AddCategory = ({setCategories}) => {
+
+const [inputValue, setInputValue] = useState('One Punch');
+const onInputChange = ({target}) => {
+    setInputValue(target.value)
+    
+}
+
+const onSubmit = (event) => {
+    event.preventDefault();
+    setCategories((categories) => [...categories,inputValue]);
+}
+  return (
+<form onSubmit={(event) => onSubmit(event)}> 
+    <input
+    type ="text"
+    placeholder="Buscar gifs"
+    value={inputValue}
+    onChange={onInputChange}
+    />
+</form>
+  
+  )
+}
